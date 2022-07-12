320fd2845c354e957462edb5b75a052ef9385eaf
ext.nikola.tadic <ext.nikola.tadic@workpuls.com>
2022-07-12 10:34:28 +0200

    inital commit

---
+++ endpoints/organizations.js
@@ -0,0 +1,9 @@
+const router = express.Router();
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+router.get('/organizations', (req, res) => {
+    
+        
+});
+
